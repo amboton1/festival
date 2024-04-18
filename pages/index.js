e34e0686dbeed6da42d4e578f1292ed331fbda2b
Ammar Botonjic <ammar.botonjic@digacon-software.com>
2024-04-18 10:16:58 +0200

    Add links and images

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -19,26 +20,48 @@ export default function Home() {
                 <span className={`${styles.line} ${styles.line3}`}></span>
             </div>
             <ul className={styles['menu-items']}>
-                <li><a href="#home">Home</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#category">Category</a></li>
-                <li><a href="#menu">Menu</a></li>
+                <li><Link href="#home">Cilj</Link></li>
+                <li><Link href="#about">Program</Link></li>
+                <li><Link href="#category">Olimpijada</Link></li>
+                <li><Link href="#menu">Lokacija</Link></li>
             </ul>
             <h1 className={styles.logo}>Navbar</h1>
         </div>
       </nav>
 
       <main>
-        <section id="home">
-            <img className={styles.image} src="https://via.placeholder.com/500" alt="Home" />
+        <section id="home" className={styles.content}>
+            <img className={styles.image} src="/fest1.png" alt="Home" />
+            <div>
+              <ul className={styles.social}>
+                <li className={styles.animated}>
+                  <a className={styles.link} href="https://web.facebook.com/WelcomeSpringFestivalZg/" target="_new">
+                  <svg height="1em" viewBox="0 0 320 512">
+                    <path
+                      d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z"
+                    ></path>
+                  </svg>
+                  </a>
+                </li>
+                <li className={styles.animated}>
+                  <a className={styles.link} href="https://www.instagram.com/welcome_spring_festival/" target="_new">
+                  <svg height="1em" viewBox="0 0 448 512">
+                    <path
+                      d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"
+                    ></path>
+                  </svg>
+                  </a>
+                </li>
+              </ul>
+            </div>
         </section>
-        <section id="about">
-            <img className={styles.image} src="https://via.placeholder.com/600" alt="About" />
+        <section id="about" className={styles.content}>
+            <img className={styles.image} src="/fest2.jpeg" alt="About" />
         </section>
-        <section id="category">
-            <img className={styles.image} src="https://via.placeholder.com/700" alt="Category" />
+        <section id="category" className={styles.content}>
+            <img className={styles.image} src="/fest3.jpg" alt="Category" />
         </section>
-        <section id="menu">
+        <section id="menu" className={styles.content}>
             <img className={styles.image} src="https://via.placeholder.com/800" alt="Menu" />
         </section>
       </main>
